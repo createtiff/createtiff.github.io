34fc0706fc4cccde070d904d3ce61cf39b3aeb60
Tiffany <createtiff@gmail.com>
2020-01-23 16:45:49 +0100

    sssss

--- win-a-mood/scripts.js
+++ win-a-mood/scripts.js
@@ -4,13 +4,17 @@ $(document).ready(function(){
 	var randomBodyColor = bodyColorArray[Math.floor(Math.random()*bodyColorArray.length)]
 	$('body').css('background-color', randomBodyColor)
 
-	var colorArray = ['cornflowerblue','crimson','teal','mediumseagreen','mediumvioletred','olive','darkolivegreen','seagreen','darkseagreen','royalblue','darkmagenta','purple','indigo','darkslateblue','firebrick']
+	var colorArray = ['palevioletred','yellowgreen','darkseagreen','mediumaquamarine','orange','lightcoral','paleturquoise','cadet blue','khaki','darkkhaki','palegoldenrod','powderblue','rosybrown','lavender','thistle','plum','orchid','mediumslateblue','lavenderblush','mistyrose','aliceblue','mintcream','honeydew','snow']
 	var randomColor = colorArray[Math.floor(Math.random()*colorArray.length)]
 	$('a').css('background-color', randomColor)
 
 	var colorHoverArray = ['palevioletred','yellowgreen','darkseagreen','mediumaquamarine','orange','lightcoral','paleturquoise','cadet blue','khaki','darkkhaki','palegoldenrod','powderblue','rosybrown','lavender','thistle','plum','orchid','mediumslateblue','lavenderblush','mistyrose','aliceblue','mintcream','honeydew','snow']
-	var randomHoverColor = colorHoverArray[Math.floor(Math.random()*colorArray.length)]
-	$('#click').css('color', randomHoverColor)
+	var randomBorderColor = colorHoverArray[Math.floor(Math.random()*colorArray.length)]
+	$('a').css('border-color', randomBorderColor)
+
+	var textColorArray = ['palevioletred','yellowgreen','darkseagreen','mediumaquamarine','orange','lightcoral','paleturquoise','cadet blue','khaki','darkkhaki','palegoldenrod','powderblue','rosybrown','lavender','thistle','plum','orchid','mediumslateblue','lavenderblush','mistyrose','aliceblue','mintcream','honeydew','snow']
+	var randomTextColor = textColorArray[Math.floor(Math.random()*colorArray.length)]
+	$('a').css('color', randomTextColor)
 
 })
 
