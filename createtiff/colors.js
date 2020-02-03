77c369282b2d61c5f4fb4c77d90ca602ceec6b90
Tiffany <createtiff@gmail.com>
2020-02-04 00:31:38 +0100

    vhjv

---
+++ createtiff/colors.js
@@ -0,0 +1,115 @@
+
+$(document).ready(function(){
+
+	var colorArray1 = ['rgba(0,200,175,1)','rgba(0,175,150,1)','rgba(0,150,150,1)','rgba(0,175,175,1)','rgba(0,175,200,1)','rgba(25,150,200,1)','rgba(50,125,200,1)','rgba(75,100,200,1)','rgba(75,75,200,1)','rgba(75,50,200,1)']
+	var randomBlue1 = colorArray1[Math.floor(Math.random()*colorArray1 .length)]
+	$('.circle-1').css('background-color', randomBlue1)
+})
+
+$(document).ready(function(){
+
+	var colorArray2 = ['rgba(150,0,150,1)','rgba(150,0,125,1)','rgba(150,0,100,1)','rgba(150,0,75,1)','rgba(150,0,50,1)','rgba(150,0,25,1)','rgba(150,0,0,1)','rgba(175,0,0,1)','rgba(200,0,0,1)','rgba(225,0,0,1)']
+	var randomRed2 = colorArray2[Math.floor(Math.random()*colorArray2 .length)]
+	$('.circle-2').css('background-color', randomRed2)
+})
+
+$(document).ready(function(){
+
+	var colorArray3 = ['rgba(150,150,0,1)','rgba(125,150,25,1)','rgba(100,125,25,1)','rgba(75,125,50,1)','rgba(50,125,75,1)','rgba(25,125,75,1)','rgba(25,100,75,1)','rgba(0,75,75,1)','rgba(0,80,75,1)','rgba(0,100,75,1)']
+	var randomGreen3 = colorArray3[Math.floor(Math.random()*colorArray3 .length)]
+	$('.circle-3').css('background-color', randomGreen3)
+})
+
+$(document).ready(function(){
+
+	var colorArray4 = ['rgba(150,150,0,1)','rgba(125,150,25,1)','rgba(100,125,25,1)','rgba(75,125,50,1)','rgba(50,125,75,1)','rgba(25,125,75,1)','rgba(25,100,75,1)','rgba(0,75,75,1)','rgba(0,80,75,1)','rgba(0,100,75,1)']
+	var randomGreen4 = colorArray4[Math.floor(Math.random()*colorArray4 .length)]
+	$('.circle-4').css('background-color', randomGreen4)
+})
+
+$(document).ready(function(){
+
+	var colorArray5 = ['rgba(150,0,150,1)','rgba(150,0,125,1)','rgba(150,0,100,1)','rgba(150,0,75,1)','rgba(150,0,50,1)','rgba(150,0,25,1)','rgba(150,0,0,1)','rgba(175,0,0,1)','rgba(200,0,0,1)','rgba(225,0,0,1)']
+	var randomRed5 = colorArray5[Math.floor(Math.random()*colorArray5 .length)]
+	$('.circle-5').css('background-color', randomRed5)
+})
+
+$(document).ready(function(){
+
+	var colorArray6 = ['rgba(0,200,175,1)','rgba(0,175,150,1)','rgba(0,150,150,1)','rgba(0,175,175,1)','rgba(0,175,200,1)','rgba(25,150,200,1)','rgba(50,125,200,1)','rgba(75,100,200,1)','rgba(75,75,200,1)','rgba(75,50,200,1)']
+	var randomBlue6 = colorArray6[Math.floor(Math.random()*colorArray6 .length)]
+	$('.circle-6').css('background-color', randomBlue6)
+})
+
+$(document).ready(function(){
+
+	var colorArray7 = ['rgba(0,200,175,1)','rgba(0,175,150,1)','rgba(0,150,150,1)','rgba(0,175,175,1)','rgba(0,175,200,1)','rgba(25,150,200,1)','rgba(50,125,200,1)','rgba(75,100,200,1)','rgba(75,75,200,1)','rgba(75,50,200,1)']
+	var randomBlue7 = colorArray7[Math.floor(Math.random()*colorArray7 .length)]
+	$('.circle-7').css('background-color', randomBlue7)
+})
+
+$(document).ready(function(){
+
+	var colorArray8 = ['rgba(150,150,0,1)','rgba(125,150,25,1)','rgba(100,125,25,1)','rgba(75,125,50,1)','rgba(50,125,75,1)','rgba(25,125,75,1)','rgba(25,100,75,1)','rgba(0,75,75,1)','rgba(0,80,75,1)','rgba(0,100,75,1)']
+	var randomGreen8 = colorArray8[Math.floor(Math.random()*colorArray8 .length)]
+	$('.circle-8').css('background-color', randomGreen8)
+})
+
+$(document).ready(function(){
+
+	var colorArray9 = ['rgba(150,0,150,1)','rgba(150,0,125,1)','rgba(150,0,100,1)','rgba(150,0,75,1)','rgba(150,0,50,1)','rgba(150,0,25,1)','rgba(150,0,0,1)','rgba(175,0,0,1)','rgba(200,0,0,1)','rgba(225,0,0,1)']
+	var randomRed9 = colorArray9[Math.floor(Math.random()*colorArray9 .length)]
+	$('.circle-9').css('background-color', randomRed9)
+})
+
+$(document).ready(function(){
+
+	var colorArray10 = ['rgba(150,0,150,1)','rgba(150,0,125,1)','rgba(150,0,100,1)','rgba(150,0,75,1)','rgba(150,0,50,1)','rgba(150,0,25,1)','rgba(150,0,0,1)','rgba(175,0,0,1)','rgba(200,0,0,1)','rgba(225,0,0,1)']
+	var randomRed10 = colorArray10[Math.floor(Math.random()*colorArray10 .length)]
+	$('.circle-10').css('background-color', randomRed10)
+})
+
+$(document).ready(function(){
+
+	var colorArray11 = ['rgba(150,150,0,1)','rgba(125,150,25,1)','rgba(100,125,25,1)','rgba(75,125,50,1)','rgba(50,125,75,1)','rgba(25,125,75,1)','rgba(25,100,75,1)','rgba(0,75,75,1)','rgba(0,80,75,1)','rgba(0,100,75,1)']
+	var randomGreen11 = colorArray11[Math.floor(Math.random()*colorArray11 .length)]
+	$('.circle-11').css('background-color', randomGreen11)
+})
+
+$(document).ready(function(){
+
+	var colorArray12 = ['rgba(0,200,175,1)','rgba(0,175,150,1)','rgba(0,150,150,1)','rgba(0,175,175,1)','rgba(0,175,200,1)','rgba(25,150,200,1)','rgba(50,125,200,1)','rgba(75,100,200,1)','rgba(75,75,200,1)','rgba(75,50,200,1)']
+	var randomBlue12 = colorArray12[Math.floor(Math.random()*colorArray12 .length)]
+	$('.circle-12').css('background-color', randomBlue12)
+})
+
+$(document).ready(function(){
+
+	var colorArray13 = ['rgba(0,200,175,1)','rgba(0,175,150,1)','rgba(0,150,150,1)','rgba(0,175,175,1)','rgba(0,175,200,1)','rgba(25,150,200,1)','rgba(50,125,200,1)','rgba(75,100,200,1)','rgba(75,75,200,1)','rgba(75,50,200,1)']
+	var randomBlue13 = colorArray13[Math.floor(Math.random()*colorArray13 .length)]
+	$('.circle-13').css('background-color', randomBlue13)
+})
+
+$(document).ready(function(){
+
+	var colorArray14 = ['rgba(150,0,150,1)','rgba(150,0,125,1)','rgba(150,0,100,1)','rgba(150,0,75,1)','rgba(150,0,50,1)','rgba(150,0,25,1)','rgba(150,0,0,1)','rgba(175,0,0,1)','rgba(200,0,0,1)','rgba(225,0,0,1)']
+	var randomRed14 = colorArray14[Math.floor(Math.random()*colorArray14 .length)]
+	$('.circle-14').css('background-color', randomRed14)
+})
+
+$(document).ready(function(){
+
+	var colorArray15 = ['rgba(150,150,0,1)','rgba(125,150,25,1)','rgba(100,125,25,1)','rgba(75,125,50,1)','rgba(50,125,75,1)','rgba(25,125,75,1)','rgba(25,100,75,1)','rgba(0,75,75,1)','rgba(0,80,75,1)','rgba(0,100,75,1)']
+	var randomGreen15 = colorArray15[Math.floor(Math.random()*colorArray15 .length)]
+	$('.circle-15').css('background-color', randomGreen15)
+})
+
+
+function myFunction() {
+  var x = document.getElementById("resultbox");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
