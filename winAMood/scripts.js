9adf7e046b042e5f373c4524242fe63b15ef6a9b
Tiffany <createtiff@gmail.com>
2021-02-13 22:31:50 +0900

    he

--- winAMood/scripts.js
+++ winAMood/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
 	var colorArray = ['cornflowerblue','crimson','teal','mediumseagreen','mediumvioletred','olive','darkolivegreen','seagreen','darkseagreen','royalblue','darkmagenta','purple','indigo','darkslateblue','firebrick']
 	var randomColor = colorArray[Math.floor(Math.random()*colorArray.length)]
-	$('a').css('background-color', randomColor)
+	$('a').css('color', randomColor)
 
 	var colorHoverArray = ['palevioletred','yellowgreen','darkseagreen','mediumaquamarine','orange','lightcoral','paleturquoise','cadet blue','khaki','darkkhaki','palegoldenrod','powderblue','rosybrown','lavender','thistle','plum','orchid','mediumslateblue','lavenderblush','mistyrose','aliceblue','mintcream','honeydew','snow']
 	var randomHoverColor = colorHoverArray[Math.floor(Math.random()*colorArray.length)]
